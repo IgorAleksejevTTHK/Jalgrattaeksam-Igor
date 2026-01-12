d43ebe9a6d92e1c4c021555b51bc0850db79aab6
IgorAleksejevTTHK <igoralekseje@gmail.com>
2026-01-12 12:55:30 +0200

    on loodud style.css ja main.php

---
+++ main.php
@@ -0,0 +1,44 @@
+<?php
+require_once("konf.php");
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Jalgrattaeksam</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>Jalgrattaeksam</h1>
+    <p>Ohutu liiklemise teadmiste ja oskuste kontroll</p>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="main.php">Avaleht</a></li>
+        <li><a href="registreerimine.php">Registreerimine</a></li>
+        <li><a href="Teooriaeksam.php">Teooriaeksam</a></li>
+        <li><a href="Slaalom.php">Slaloom</a></li>
+        <li><a href="Ringtee.php">Ringtee</a></li>
+        <li><a href="Tänav.php">Tänavasõit</a></li>
+        <li><a href="Lubadeleht.php">Lubade leht</a></li>
+    </ul>
+</nav>
+
+<main>
+    <h2>Tere tulemast jalgrattaeksamile!</h2>
+    <p>
+        Sellel veebilehel saad registreeruda jalgrattaeksamile ning tutvuda
+        teooria ja praktiliste harjutustega.
+    </p>
+
+    <img src="bike.png" alt="Jalgratas" style="max-width:300px;">
+</main>
+
+
+
+</body>
+</html>
